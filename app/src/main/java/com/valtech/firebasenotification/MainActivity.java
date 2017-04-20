d9aeb6b958c13c6199da5d8ddeff618d074dcb0e
Oleksii Dobrunov <lamrak@gmail.com>
2017-04-20 15:01:25 +0300

    Initial commit

---
+++ app/src/main/java/com/valtech/firebasenotification/MainActivity.java
@@ -0,0 +1,25 @@
+package com.valtech.firebasenotification;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get token
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        // Log and toast
+        String msg = getString(R.string.msg_token_fmt, token);
+        Log.d(MainActivity.class.getSimpleName(), msg);
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
